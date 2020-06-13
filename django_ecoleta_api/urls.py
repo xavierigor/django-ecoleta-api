e8d50288099d05780103c75f6a216b240a1fba03
Igor Xavier <igoorlimaa@gmail.com>
2020-06-13 17:43:54 -0300

    Remove unused admin url, app and file

--- django_ecoleta_api/urls.py
+++ django_ecoleta_api/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
@@ -8,6 +7,5 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
 
     path('api/', include('coleta.urls')),
-    path('admin/', admin.site.urls),
-    
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
