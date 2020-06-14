96a46359c3ebddcf0cc3a1ae85ae07697540dcff
Igor Xavier <igoorlimaa@gmail.com>
2020-06-13 22:54:39 -0300

    refactor: rearranging the projects structure

---
+++ django_ecoleta_api/settings.py
@@ -0,0 +1,127 @@
+import environ
+
+env = environ.Env()
+root_path = environ.Path(__file__) - 2
+env.read_env(env_file=root_path(".env"))
+
+
+# -----------------------------------------------------------------------------
+# Basic Config
+# -----------------------------------------------------------------------------
+ENV = env("ENV", default="prod")
+assert ENV in ["dev", "test", "prod", "qa"]
+
+DEBUG = env.bool("DEBUG", default=False)
+
+BASE_DIR = root_path()
+
+ROOT_URLCONF = "django_ecoleta_api.urls"
+
+WSGI_APPLICATION = "django_ecoleta_api.wsgi.application"
+
+
+# -----------------------------------------------------------------------------
+# Time & Language
+# -----------------------------------------------------------------------------
+LANGUAGE_CODE = env("LANGUAGE_CODE", default="en-us")
+TIME_ZONE = env("TIMEZONE", default="UTC")
+USE_I18N = env("USE_I18N", default=True)
+USE_L10N = env("USE_L10N", default=True)
+USE_TZ = env("USE_TZ", default=True)
+
+
+# -----------------------------------------------------------------------------
+# Emails
+# -----------------------------------------------------------------------------
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="")
+EMAIL_BACKEND = env(
+    "EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
+)
+
+
+# -----------------------------------------------------------------------------
+# Security and Users
+# -----------------------------------------------------------------------------
+SECRET_KEY = env("SECRET_KEY")
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[])
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+
+# -----------------------------------------------------------------------------
+# Databases
+# -----------------------------------------------------------------------------
+DJANGO_DATABASE_URL = env.db("DATABASE_URL")
+DATABASES = {"default": DJANGO_DATABASE_URL}
+
+
+# -----------------------------------------------------------------------------
+# Applications configuration
+# -----------------------------------------------------------------------------
+INSTALLED_APPS = [
+    # First party
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # Third party
+    "django_filters",
+    "rest_framework",
+
+    # Local
+    "coleta"
+]
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [root_path("templates")],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+# -----------------------------------------------------------------------------
+# Static & Media Files
+# -----------------------------------------------------------------------------
+STATIC_URL = env("STATIC_URL", default="/static/")
+STATIC_ROOT = env("STATIC_ROOT", default=root_path("static"))
+
+MEDIA_URL = env("MEDIA_URL", default="/media/")
+MEDIA_ROOT = env("MEDIA_ROOT", default=root_path("media"))
+
+
+# -----------------------------------------------------------------------------
+# Django Rest Framework configuration
+# -----------------------------------------------------------------------------
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
